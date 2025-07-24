1690aebe23680c6014daa556af39c32a2c8527a7
Cloud Enabler <markus.jacob@devoteam.com>
2025-07-24 10:40:23 +0200

    Squash merge feature-branch-name into main

--- modules/azuread/credentials/keyvault_secrets.tf
+++ modules/azuread/credentials/keyvault_secrets.tf
@@ -1,9 +1,11 @@
-
 resource "azurerm_key_vault_secret" "client_id" {
-  for_each = try(var.settings.keyvaults, {})
+  for_each = {
+    for k, v in try(var.settings.keyvaults, {}) : k => v
+    if k != "lz_key" && can(v.secret_prefix)
+  }
 
   name         = format("%s-client-id", each.value.secret_prefix)
-  key_vault_id = try(each.value.lz_key, null) == null ? var.keyvaults[var.client_config.landingzone_key][each.key].id : var.keyvaults[each.value.lz_key][each.key].id
+  key_vault_id = try(var.settings.keyvaults.lz_key, null) == null ? var.keyvaults[var.client_config.landingzone_key][each.key].id : var.keyvaults[var.settings.keyvaults.lz_key][each.key].id
 
   value = coalesce(
     try(var.resources.application.client_id, null)
@@ -11,9 +13,12 @@ resource "azurerm_key_vault_secret" "client_id" {
 }
 
 resource "azurerm_key_vault_secret" "tenant_id" {
-  for_each     = try(var.settings.keyvaults, {})
+  for_each = {
+    for k, v in try(var.settings.keyvaults, {}) : k => v
+    if k != "lz_key" && can(v.secret_prefix)
+  }
+  
   name         = format("%s-tenant-id", each.value.secret_prefix)
   value        = try(each.value.tenant_id, var.client_config.tenant_id)
-  key_vault_id = try(each.value.lz_key, null) == null ? var.keyvaults[var.client_config.landingzone_key][each.key].id : var.keyvaults[each.value.lz_key][each.key].id
+  key_vault_id = try(var.settings.keyvaults.lz_key, null) == null ? var.keyvaults[var.client_config.landingzone_key][each.key].id : var.keyvaults[var.settings.keyvaults.lz_key][each.key].id
 }
-
--- modules/azuread/credentials/password.tf
+++ modules/azuread/credentials/password.tf
@@ -75,7 +75,7 @@ resource "time_sleep" "wait_new_password_propagation" {
 resource "azurerm_key_vault_secret" "client_secret" {
   for_each = {
     for key, value in try(var.settings.keyvaults, {}) : key => value
-    if try(var.settings.azuread_application, null) != null && lower(local.password_type) == "password"
+    if key != "lz_key" && can(value.secret_prefix) && try(var.settings.azuread_application, null) != null && lower(local.password_type) == "password"
   }
 
   # Add a timer to make sure the new password got replicated into azure ad replica set before we store it into keyvault
@@ -83,7 +83,7 @@ resource "azurerm_key_vault_secret" "client_secret" {
 
   name            = format("%s-client-secret", each.value.secret_prefix)
   value           = local.random_key == "key0" ? sensitive(azuread_application_password.key0.0.value) : try(sensitive(azuread_application_password.key1.0.value), sensitive(azuread_application_password.key.0.value))
-  key_vault_id    = try(each.value.lz_key, null) == null ? var.keyvaults[var.client_config.landingzone_key][each.key].id : var.keyvaults[each.value.lz_key][each.key].id
+  key_vault_id    = try(var.settings.keyvaults.lz_key, null) == null ? var.keyvaults[var.client_config.landingzone_key][each.key].id : var.keyvaults[var.settings.keyvaults.lz_key][each.key].id
   expiration_date = local.random_key == "key0" ? local.expiration_date.key0 : try(local.expiration_date.key1, local.expiration_date.key)
 
   tags = {
